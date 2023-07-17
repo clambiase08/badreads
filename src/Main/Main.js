f345e7a2120460d561196fc16cc26b765a32e35b
Christina Lambiase <christina.lambiase@gmail.com>
2023-07-17 14:37:28 -0700

    main updated

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import About from './About';
 import Account from './Account';
 
-
 export default function Main() {
+  const [books, setBooks] = useState([]);
+  const key = 'AIzaSyAkjPer_Cb-OAsgHXPMbRH6I88BGgpQSWg'
+  useEffect(() => {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${key}`)
+    .then((res) => res.json())
+    .then((BookItem) => setBooks(BookItem))
+  }, [])
+
+
+  
   return (
     <div>
-      
+      <About books={books}/>
     </div>
-  )
+  );
 }
