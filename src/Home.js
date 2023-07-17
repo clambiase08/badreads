b0858f528ea9b29e123b8c1e68434d9b9339421a
Christina Lambiase <christina.lambiase@gmail.com>
2023-07-17 10:13:53 -0700

    basic home component complete

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import image1 from './assets/book-table-home.png'
+import image2 from './assets/books-home.png'
+import { BsChatRightText } from'react-icons/bs'
+import { BsStar } from'react-icons/bs'
+
 
 export default function Home() {
   return (
     <div>
-      
+      <h1>Your safe space for really bad books</h1>
+      <span>
+        <img src={image2} />
+        <img src={image1} />
+      </span>
+      <div>
+        <span>
+          <h4><BsChatRightText /> Your rants, available online</h4>
+          <p>Be honest about your least favorite books and tell us how you really feel</p>
+          </span>
+          <span>
+          <h4><BsStar /> Creating Space</h4>
+          <p>Get out those bad feelings with personalized reviews, ratings, and attribute tags</p>
+          </span>
+      </div>
+
     </div>
   )
 }
