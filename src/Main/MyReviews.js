4250c36bd7d462b902f2f580ec7b29566ae57132
sandyjtech <sandyjtech@gmail.com>
2023-07-18 13:05:54 -0700

    added account styles

--- src/Main/MyReviews.js
+++ src/Main/MyReviews.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import Review from './Review'
+import styled from "styled-components";
 
-export default function MyReviews({reviews}) {
+const StyledDiv = styled.div`
+  text-align: center; /* Center-align the text within the div */
+`;
 
+const StyledSpan = styled.span`
+  color: gray;
+  font-size: 14px;
+  margin-top: 10px;
+`;
 
+const StyledH1 = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  padding: 5px;
+`;
+
+export default function MyReviews({ reviews }) {
   return (
-    <div>
-      {reviews.map((reviewItem) =>(
-        <Review key={reviewItem.id} reviewItem={reviewItem}/>
+    <StyledDiv> {/* Wrap the content in a div and apply the center alignment */}
+      <StyledSpan>#READERS TELL READERS</StyledSpan>
+      <StyledH1>Your Shittiest reviews</StyledH1>
+      {reviews.map((reviewItem) => (
+        <Review key={reviewItem.id} reviewItem={reviewItem} />
       ))}
-    </div>
-  )
+    </StyledDiv>
+  );
 }
--- src/Main/Review.js
+++ src/Main/Review.js
@@ -7,20 +7,39 @@ export default function Review({ reviewItem }) {
   const ReviewContainer = styled.div`
     width: 80%;
     border: 2px dotted black;
-    background: ${props => (props.index % 2 === 0 ? 'yellow' : 'white')};
+    background: ${(props) => (props.index % 2 === 0 ? "#FCF5EA" : "white")};
+    margin: 10px auto;
+    display: flex;
+    padding: 10px;
+      overflow: auto; /* Enable text wrapping */
+  `;
+
+  const ImageContainer = styled.div`
+    flex: 0 0 30%; 
+  `;
+
+  const TextContainer = styled.div`
+    flex: 0 0 70%; /* Adjust the width as needed */
+    padding-left: 10px; /* Add padding between image and text */
+  `;
+
+  const Image = styled.img`
+    width: 40%;
   `;
 
   return (
     <ReviewContainer index={id}>
-      <span>#READERS TELL READERS</span>
-      <h1>Your Shittiest reviews</h1>
-      <section>
-        <span>{tags}</span>
-        <p>{rating}</p>
-        <h2>{title}</h2>
-        <p>{review}</p>
-      </section>
-      <img src={image} alt="Review" style={{ width: '20px' }} />
+      <ImageContainer>
+        <Image src={image} alt="Review" />
+      </ImageContainer>
+      <TextContainer>
+        <section>
+          <span>{tags}</span>
+          <p>{rating}</p>
+          <h2>{title}</h2>
+          <p>{review}</p>
+        </section>
+      </TextContainer>
     </ReviewContainer>
   );
 }
