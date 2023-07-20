687f31319c01d91d72ca5f56b8704c21b290ac6b
Christina Lambiase <christina.lambiase@gmail.com>
2023-07-20 09:18:19 -0700

    small styling changes

--- src/Main/About.js
+++ src/Main/About.js
@@ -3,22 +3,6 @@ import React from "react";
 import AboutBookRow from "./AboutBookRow";
 import { styled } from "styled-components";
 
-const Header = styled.header`
-  text-align: center;
-  margin: 0 auto;
-  padding-bottom: 50px;
-  font-size: 1.5em;
-`;
-const Hashtag = styled.p`
-  text-align: center;
-  margin: 0 auto;
-  padding-top: 70px;
-  font-weight: bold;
-  font-size: 10px;
-`;
-const Container = styled.div`
-background-color: rgba(252, 245, 234, 1);
-`;
 
 export default function About() {
   return (
@@ -30,3 +14,20 @@ export default function About() {
     </Container>
   );
 }
+
+  const Header = styled.header`
+    text-align: center;
+    margin: 0 auto;
+    padding-bottom: 50px;
+    font-size: 46px;
+    font-weight: 300;
+  `;
+  const Hashtag = styled.p`
+    text-align: center;
+    margin: 0 auto;
+    padding-top: 70px;
+    font-size: 14px;
+  `;
+  const Container = styled.div`
+  background-color: rgba(252, 245, 234, 1);
+  `;
