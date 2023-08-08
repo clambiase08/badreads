da10aa93f8259acd0c2762b3a0c014e7a890ec1e
sandyjtech <sandyjtech@gmail.com>
2023-08-08 15:47:29 -0700

    Updates

---
+++ precache-manifest.afbe1158feacd081f5e7633366045972.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d07e1df54e5259443d6",
+    "url": "/badreads/static/css/main.8c8ddaf2.chunk.css"
+  },
+  {
+    "revision": "5d07e1df54e5259443d6",
+    "url": "/badreads/static/js/main.5d07e1df.chunk.js"
+  },
+  {
+    "revision": "bfa17f515437437af62a",
+    "url": "/badreads/static/js/1.bfa17f51.chunk.js"
+  },
+  {
+    "revision": "6bd4d1d954c077f2f355",
+    "url": "/badreads/static/js/2.6bd4d1d9.chunk.js"
+  },
+  {
+    "revision": "5bee437100e82a1eb174",
+    "url": "/badreads/static/js/runtime~main.5bee4371.js"
+  },
+  {
+    "revision": "bd3eded4a176d64a644fcb9f6a18c5ca",
+    "url": "/badreads/static/media/book-contactv2.bd3eded4.png"
+  },
+  {
+    "revision": "e327f12a5ba3c7d3619bf3abda3e9fb6",
+    "url": "/badreads/static/media/book-table-home.e327f12a.png"
+  },
+  {
+    "revision": "22a497739a04b90709d9bbec165f5502",
+    "url": "/badreads/static/media/books-home.22a49773.png"
+  },
+  {
+    "revision": "96a969bb6d3442bdff1682655ad67b2c",
+    "url": "/badreads/index.html"
+  }
+];
